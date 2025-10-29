da2c8526a152bf4cfa63eea807085e0bbd35ca7e
Mark Harrison <dataless@gmail.com>
2025-10-29 18:51:53 +0000

    added back button to 404 page

--- src/app/components/wrong-route/wrong-route.ts
+++ src/app/components/wrong-route/wrong-route.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-wrong-route',
-  imports: [],
+  imports: [MatButtonModule],
+  standalone: true,
   templateUrl: './wrong-route.html',
   styleUrl: './wrong-route.scss',
 })
 export class WrongRoute {
+  constructor(private location: Location) {}
 
+  goBack(): void {
+    this.location.back();
+  }
 }
